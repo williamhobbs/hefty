7c3a962695debcc24e2024467251ed4c4488e9d3
Will Hobbs <45701090+williamhobbs@users.noreply.github.com>
2026-04-09 13:00:05 -0500

    add modules to init, closes #30

--- src/hefty/__init__.py
+++ src/hefty/__init__.py
@@ -1,3 +1,11 @@
 import importlib.metadata
 
 __version__ = importlib.metadata.version(__package__ or __name__)
+
+from hefty import(
+    solar,
+    pv_model,
+    utilities,
+    wind,
+    custom,
+)
